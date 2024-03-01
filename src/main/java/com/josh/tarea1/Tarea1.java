5105c39bd705b0ffe68aae94b4f86949eafc85ab
Josh <144963859+JoshuaBarrantesMena@users.noreply.github.com>
2024-03-01 16:14:52 -0600

    repository test
    Only a test changing the repository name

--- src/main/java/com/josh/tarea1/Tarea1.java
+++ src/main/java/com/josh/tarea1/Tarea1.java
@@ -11,7 +11,7 @@ public class Tarea1 {
         
         String initDialog = "Bienvenido a la calculadoraDeJava-Inador\n\n";
                initDialog = initDialog.concat("Esta calculadora es capaz de realizar operaciones como\n");
-               initDialog = initDialog.concat("sumar, restar, multiplicar y dividir.\n");
+               initDialog = initDialog.concat("sumar, restar, multiplicar y dividir.\n\n");
                
         JOptionPane.showMessageDialog(null, initDialog);
     }
