a38d6bc15dc0e65fd4eca0c7bb1423a4d02bf52c
Josh <144963859+JoshuaBarrantesMena@users.noreply.github.com>
2024-03-02 23:41:51 -0600

    Calculator cycle
    the calculator cycle is working, can choose the options of the calculator, include re-elect the numbers and exit application

--- src/main/java/com/josh/tarea1/Calculator.java
+++ src/main/java/com/josh/tarea1/Calculator.java
@@ -13,17 +13,53 @@ public class Calculator {
                initDialog = initDialog.concat("Esta calculadora es capaz de realizar operaciones como\n");
                initDialog = initDialog.concat("sumar, restar, multiplicar y dividir.\n\n");
                
-        JOptionPane.showMessageDialog(null, initDialog);
-          
         String rulesDialog = "reglas para evitar errores";
-        
+               
+        JOptionPane.showMessageDialog(null, initDialog);
         JOptionPane.showMessageDialog(null, rulesDialog);
         
-        int num1 = 2, num2 = 4;
+        int num1, num2, option;
+        
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer numero"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo numero"));
         
         CalcEngine numbers = new CalcEngine(num1, num2);
         
-        JOptionPane.showMessageDialog(null, numbers.multiplyNumbers()); //test
-        //JOptionPane.showMessageDialog(null, "El numero 1 debe ser diferente de '0'");
+        String[] options = {"Cambiar Numeros", "Sumar", "Restar", "Multiplicar", "Dividir", "Salir"};
+        
+        boolean calcCycle = true;
+        
+        while(calcCycle){
+            
+            option = JOptionPane.showOptionDialog(null,"Que desea hacer?", "Escojer opcion", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null , options, options[0]);
+        
+            switch(option){
+                case (0) -> {
+                    num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer numero"));
+                    num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo numero"));
+                    numbers.resetNumbers(num1, num2);
+                }
+                case (1) -> {
+                    JOptionPane.showMessageDialog(null, numbers.addNumbers());
+               
+                }
+                case (2) -> {
+                    JOptionPane.showMessageDialog(null, numbers.substractNumbers());
+                }
+                case (3) -> {
+                    JOptionPane.showMessageDialog(null, numbers.multiplyNumbers());
+                }
+                case (4) -> {
+                    if (num1 != 0){
+                        JOptionPane.showMessageDialog(null, numbers.splitNumbers());
+                    }else{
+                        JOptionPane.showMessageDialog(null, "El primer numero debe ser diferente de '0'");
+                    }
+                }
+                case (5) -> {
+                    calcCycle = false;
+                }
+            }
+        }
     }
 }
